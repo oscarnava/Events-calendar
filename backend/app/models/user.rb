05b96ce4eb622a9980f884615d778eeaf1fdfffe
oscarnava <oscarnava@gmail.com>
2019-12-09 16:02:21 -0600

    Add model validations

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :email, presence: true, format: /\w+@\w+\.{1}[a-zA-Z]{2,}/
+
+  has_many :user_events
 end
--- backend/app/models/userevent.rb
+++ backend/app/models/userevent.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Userevent < ApplicationRecord
   belongs_to :user
   belongs_to :event
