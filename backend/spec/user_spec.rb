9d060fcf38f58b62c35ef7602d165a0a045db8fd
oscarnava <oscarnava@gmail.com>
2019-12-10 15:27:55 -0600

    Update tests

--- backend/spec/user_spec.rb
+++ backend/spec/user_spec.rb
@@ -10,11 +10,9 @@ RSpec.describe 'Users management', type: :request do
     }
   end
 
-  before do
-    User.find_by_name('user01')&.delete
-  end
-
   it 'Creates a new user' do
+    User.find_by_name('user01')&.delete
+
     post '/users?name=user01&email=user01@test.com', headers: headers
 
     expect(response.content_type).to eq('application/json; charset=utf-8')
@@ -26,19 +24,20 @@ RSpec.describe 'Users management', type: :request do
     expect(resp['user']['id']).to be
   end
 
-  it 'Read the new user' do
+  it 'Get the user by name' do
     user = User.all.last
     Event.all.each { |event| user.userevents.create(event: event) }
 
-    get "/users/#{user.id}", headers: headers
+    get "/users/#{user.name}", headers: headers
 
     expect(response.content_type).to eq('application/json; charset=utf-8')
     expect(response).to have_http_status(:success)
 
     resp = JSON.parse(response.body)
+    # warn resp
 
     expect(resp['status']).to eq('ok')
     expect(resp['name']).to eq(user.name)
-    expect(resp['events']).to eq(user.userevents.all.map(&:id))
+    expect(resp['events']).to eq(user.userevents.all.map(&:event_id))
   end
 end
