f565cb187593775e9ae8e57cfb614384704afaa4
oscarnava <oscarnava@gmail.com>
2019-12-14 12:34:21 -0600

    Add build to repo

---
+++ precache-manifest.db1077037479f703e7182cd1df3ee327.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12e1f98070093ca910a6eb6995fac934",
+    "url": "./index.html"
+  },
+  {
+    "revision": "197721b7a853c780e54c",
+    "url": "./static/css/main.53708945.chunk.css"
+  },
+  {
+    "revision": "5dd896a32c4ba4e3ee10",
+    "url": "./static/js/2.22bceda3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.22bceda3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "197721b7a853c780e54c",
+    "url": "./static/js/main.f1f19990.chunk.js"
+  },
+  {
+    "revision": "9f7f031a722dcd45ae62",
+    "url": "./static/js/runtime-main.de284e28.js"
+  }
+]);
