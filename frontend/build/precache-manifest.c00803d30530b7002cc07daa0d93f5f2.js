7ab6454bf364b2012828c9525250bbf3f103cad0
oscarnava <oscarnava@gmail.com>
2019-12-16 13:56:02 -0600

    New build

---
+++ frontend/build/precache-manifest.c00803d30530b7002cc07daa0d93f5f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e9fd591127f0ed347342dbcf41470fd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7b5dee6d5142afb852d0",
+    "url": "./static/css/main.64059192.chunk.css"
+  },
+  {
+    "revision": "4bc78c5491a163885aa6",
+    "url": "./static/js/2.1c21fd99.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "./static/js/2.1c21fd99.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7b5dee6d5142afb852d0",
+    "url": "./static/js/main.09bca580.chunk.js"
+  },
+  {
+    "revision": "9f7f031a722dcd45ae62",
+    "url": "./static/js/runtime-main.de284e28.js"
+  }
+]);
