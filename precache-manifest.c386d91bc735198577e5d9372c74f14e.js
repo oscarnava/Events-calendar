878f1381e916bd3cf59aa7402cf30e041241107a
oscarnava <oscarnava@gmail.com>
2019-12-17 10:02:07 -0600

    Rebuild

--- precache-manifest.c386d91bc735198577e5d9372c74f14e.js
+++ precache-manifest.c386d91bc735198577e5d9372c74f14e.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0e9fd591127f0ed347342dbcf41470fd",
+    "revision": "e7ec2e7c3ec15755b6ae55a9f8bc79c3",
     "url": "./index.html"
   },
   {
-    "revision": "7b5dee6d5142afb852d0",
-    "url": "./static/css/main.64059192.chunk.css"
+    "revision": "6f80a0f36ef80077b515",
+    "url": "./static/css/main.7c378dd3.chunk.css"
   },
   {
     "revision": "4bc78c5491a163885aa6",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.1c21fd99.chunk.js.LICENSE"
   },
   {
-    "revision": "7b5dee6d5142afb852d0",
-    "url": "./static/js/main.09bca580.chunk.js"
+    "revision": "6f80a0f36ef80077b515",
+    "url": "./static/js/main.3b63a46c.chunk.js"
   },
   {
     "revision": "9f7f031a722dcd45ae62",
